6f9df28d46fe9e73508335363f6ef90ab93dbb83
imhuis <77392576@qq.com>
2021-09-17 19:21:25 +0800

    新增依赖

--- src/main/java/com/tencent/nft/mapper/WxUserMapper.java
+++ src/main/java/com/tencent/nft/mapper/WxUserMapper.java
@@ -1,12 +1,14 @@
 package com.tencent.nft.mapper;
 
 import com.tencent.nft.entity.security.WxUser;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author: imhuis
  * @date: 2021/9/17
  * @description:
  */
+@Mapper
 public interface WxUserMapper {
 
     WxUser selectByPhone(String phone);
