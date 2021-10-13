fc976a57337f6cd6774ae84991c64c4a7c7bc9f1
imhuis <77392576@qq.com>
2021-10-13 17:15:15 +0800

    2021-10-13 4

--- src/main/java/com/tencent/nft/service/pay/StockService.java
+++ src/main/java/com/tencent/nft/service/pay/StockService.java
@@ -1,11 +1,8 @@
 package com.tencent.nft.service.pay;
 
-import com.tencent.nft.mapper.pay.ProductMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
